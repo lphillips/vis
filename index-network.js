53ad0c65e212af87ffe001f690b1a7213fe6b68a
Luke Phillips <luke.phillips@nextissuemedia.com>
2016-05-09 11:52:34 -0700

    Export Shape classes from library for extension by clients.
    Exporting the Shape classes makes implementing custom shapes easier.

--- index-network.js
+++ index-network.js
@@ -17,6 +17,25 @@ exports.network = {
 };
 exports.network.convertDot   = function (input) {return exports.network.dotparser.DOTToGraph(input)};
 exports.network.convertGephi = function (input,options) {return exports.network.gephiParser.parseGephi(input,options)};
+exports.network.shapes = {
+  CircleImageBase: require('./lib/network/modules/components/nodes/util/CircleImageBase'),
+  NodeBase: require('./lib/network/modules/components/nodes/util/NodeBase'),
+  ShapeBase: require('./lib/network/modules/components/nodes/util/ShapeBase'),
+  Box: require('./lib/network/modules/components/nodes/shapes/Box'),
+  Circle: require('./lib/network/modules/components/nodes/shapes/Circle'),
+  CircularImage: require('./lib/network/modules/components/nodes/shapes/CircularImage'),
+  Database: require('./lib/network/modules/components/nodes/shapes/Database'),
+  Diamond: require('./lib/network/modules/components/nodes/shapes/Diamond'),
+  Dot: require('./lib/network/modules/components/nodes/shapes/Dot'),
+  Ellipse: require('./lib/network/modules/components/nodes/shapes/Ellipse'),
+  Icon: require('./lib/network/modules/components/nodes/shapes/Icon'),
+  Image: require('./lib/network/modules/components/nodes/shapes/Image'),
+  Square: require('./lib/network/modules/components/nodes/shapes/Square'),
+  Star: require('./lib/network/modules/components/nodes/shapes/Star'),
+  Text: require('./lib/network/modules/components/nodes/shapes/Text'),
+  Triangle: require('./lib/network/modules/components/nodes/shapes/Triangle'),
+  TriangleDown: require('./lib/network/modules/components/nodes/shapes/TriangleDown')
+};
 
 // bundled external libraries
 exports.moment = require('./lib/module/moment');
